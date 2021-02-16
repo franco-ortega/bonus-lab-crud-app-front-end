7f3e12b08166c1bb90b8a7e376350a4c070a81c0
Franco Ortega <francopdx@gmail.com>
2021-02-15 16:35:15 -0800

    add initial folders and files

---
+++ src/components/app/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import MiceList from '../../containers/MiceList';
+
+export default function App() {
+  return (
+    <MiceList />
+  );
+}
---
+++ src/containers/MiceList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import MouseDetails from '../components/mice/MouseDetails';
+
+export default function MiceList() {
+  return (
+    <div>
+      A list of mice:
+      <MouseDetails />
+    </div>
+  );
+}
