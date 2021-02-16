215b441b77947b321f273210fcf7ed5d9b2382a8
Franco Ortega <francopdx@gmail.com>
2021-02-15 17:44:59 -0800

    add state useState and useEffect to App.jsx; display data on MiceList.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getMice } from '../../services/mouseRequest';
 import MiceList from '../../containers/MiceList';
 
 export default function App() {
+  const [mice, setMice] = useState([]);
+
+  useEffect(async() => {
+    return getMice()
+      .then(mice => setMice(mice));
+  }, []);
+  
+  console.log('Mice (GET response):');
+  console.log(mice);
+
+
   return (
-    <MiceList />
+    <MiceList mice={mice} />
   );
 }
--- src/containers/MiceList.jsx
+++ src/containers/MiceList.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MouseDetails from '../components/mice/MouseDetails';
 
-export default function MiceList() {
+export default function MiceList({ mice }) {
+  console.log(mice);
+
+  const miceElements = mice.map(mouse => (
+    <li key={mouse.id}>
+      <p>
+        {mouse.name}
+      </p>
+      <p>
+        {mouse.furColor}
+      </p>
+      <p>
+        {mouse.tailLength}
+      </p>
+    </li>
+  ));
   return (
-    <div>
+    <ul>
       A list of mice:
-      <MouseDetails />
-    </div>
+      {/* <MouseDetails /> */}
+      {miceElements}
+    </ul>
   );
 }
+
+MiceList.propTypes = {
+  mice: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    furColor: PropTypes.string.isRequired,
+    tailLength: PropTypes.number.isRequired,
+  })).isRequired
+};
